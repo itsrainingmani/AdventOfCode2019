9d858cdd4ff478f03ef72a1a8f4bbe499e19f7f8
Manikandan Sundararajan <mani@hey.com>
2020-11-22 02:13:57 -0500

    Add convenience functions for getting a slice, updating value at index

--- src/adventofcode/utils.clj
+++ src/adventofcode/utils.clj
@@ -5,5 +5,18 @@
   (let [curr-ns (second (str/split (str *ns*) #"\."))]
     (str "data/" curr-ns ".txt")))
 
-(defn get-input []
-  (str/split-lines (slurp (input-file))))
+(defn get-input
+  ([] (str/split-lines (slurp (input-file))))
+  ([separator] (str/split (str/trim (slurp (input-file))) separator)))
+
+(defn convert-num [inp]
+  (map #(Integer/parseInt %) inp))
+
+(defn slice [i n coll]
+  (let [last (+ i n)]
+    (take-last n (take last coll))))
+
+(defn inplace [i v coll]
+  (if (>= i (count coll))
+    (list v)
+    (concat (take i coll) (list v) (drop (inc i) coll))))
