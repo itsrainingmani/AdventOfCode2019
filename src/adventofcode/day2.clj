1832c205522224345d3c17b1bc371533fcf25814
Manikandan Sundararajan <mani@hey.com>
2020-11-22 04:02:18 -0500

    Solve day 2 part 2

--- src/adventofcode/day2.clj
+++ src/adventofcode/day2.clj
@@ -11,16 +11,28 @@
 ;; Opcode 2 - same as opcode 1 but mults instead of add
 ;; Opcode 99 Halts the program
 
-(defn intcode []
-  (let [inp (utils/convert-num (utils/get-input #","))]
-    (loop [program (utils/inplace 2 2 (utils/inplace 1 12 inp))
+(defn intcode [pair]
+  (let [noun (first pair)
+        verb (second pair)
+        inp (utils/convert-num (utils/get-input #","))]
+    (loop [program (utils/inplace 2 verb (utils/inplace 1 noun inp))
            curpos 0]
       (let [ops (utils/slice curpos 4 program)
             x (nth program (nth ops 1))
             y (nth program (nth ops 2))
             newpos (nth ops 3)]
-        (println ops x y)
+        ;; (println ops x y)
         (case (nth program curpos)
           1  (recur (utils/inplace newpos (+ x y) program) (+ curpos 4))
           2  (recur (utils/inplace newpos (* x y) program) (+ curpos 4))
-          99 program)))))
+          99 (first program))))))
+
+(defn input-pair []
+  (let [pairs (for [noun (range 99)
+                    verb (range 99)]
+                [noun verb])]
+    (loop [curpos 0]
+      (if (= 19690720 (intcode (nth pairs curpos)))
+        (nth pairs curpos)
+        (recur (inc curpos))))))
+;; [67 18]
